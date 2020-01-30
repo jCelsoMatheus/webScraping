51203207c2fcdfa59d963a0215bb25962a0f4a12
jCelsoMatheus <Celso_rorato@hotmail.com>
2020-01-30 13:18:47 -0300

    Projeto Web Scraping

---
+++ WFAWebScraping/WFAWebScraping/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WFAWebScraping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Titulo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Postagem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Exibicao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Resposta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Link = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.AtualizarButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Titulo,
+            this.Postagem,
+            this.Exibicao,
+            this.Resposta,
+            this.Link});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 98);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(794, 380);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Titulo
+            // 
+            this.Titulo.HeaderText = "Titulo";
+            this.Titulo.Name = "Titulo";
+            this.Titulo.Width = 300;
+            // 
+            // Postagem
+            // 
+            this.Postagem.HeaderText = "Postagem";
+            this.Postagem.Name = "Postagem";
+            this.Postagem.Width = 150;
+            // 
+            // Exibicao
+            // 
+            this.Exibicao.HeaderText = "Exibições";
+            this.Exibicao.Name = "Exibicao";
+            // 
+            // Resposta
+            // 
+            this.Resposta.HeaderText = "Respostas";
+            this.Resposta.Name = "Resposta";
+            // 
+            // Link
+            // 
+            this.Link.HeaderText = "Link";
+            this.Link.Name = "Link";
+            // 
+            // AtualizarButton
+            // 
+            this.AtualizarButton.Location = new System.Drawing.Point(12, 36);
+            this.AtualizarButton.Name = "AtualizarButton";
+            this.AtualizarButton.Size = new System.Drawing.Size(130, 37);
+            this.AtualizarButton.TabIndex = 1;
+            this.AtualizarButton.Text = "Atualizar Lista";
+            this.AtualizarButton.UseVisualStyleBackColor = true;
+            this.AtualizarButton.Click += new System.EventHandler(this.AtualizarButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 502);
+            this.Controls.Add(this.AtualizarButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Titulo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Postagem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Exibicao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Resposta;
+        private System.Windows.Forms.DataGridViewLinkColumn Link;
+        private System.Windows.Forms.Button AtualizarButton;
+    }
+}
+
---
+++ WFAWebScraping/WFAWebScraping/Form1.cs
@@ -0,0 +1,72 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAWebScraping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AtualizarButton_Click(object sender, EventArgs e)
+        {
+            var wc = new WebClient();
+            string pagina = wc.DownloadString("https://social.msdn.microsoft.com/Forums/pt-BR/home?filter=alltypes&sort=firstpostdesc");
+
+            var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+            htmlDocument.LoadHtml(pagina);
+
+            dataGridView1.Rows.Clear();
+
+            string id = string.Empty;
+            string titulo = string.Empty;
+            string postagem = string.Empty;
+            string exibicao = string.Empty;
+            string resposta = string.Empty;
+            string link = string.Empty;
+
+            //Foreach para capturar cada comentário individualmente
+            // "threadList" é o ID da lista na página HTML
+            foreach (HtmlNode node in htmlDocument.GetElementbyId("threadList").ChildNodes)
+            {
+                // Veirificar se o meu Elemento (Node) tem algum atributo
+                if (node.Attributes.Count > 0)
+                {
+                    id = node.Attributes["data-threadid"].Value;
+                    //Copiar o link fixo na página e adicionar o id
+                    link = "https://social.msdn.microsoft.com/Forums/pt-BR/" + id;
+                    titulo = node.Descendants().First(x => x.Id.Equals("threadTitle_" + id)).InnerText;
+                    postagem = WebUtility.HtmlDecode(node.Descendants().First(x => x.Attributes["class"] != null && x.Attributes["class"].Value.Equals("lastpost")).InnerText.Replace("\n", "").Replace("  ", ""));
+                    exibicao = WebUtility.HtmlDecode(node.Descendants().First(x => x.Attributes["class"] != null && x.Attributes["class"].Value.Equals("viewcount")).InnerText);
+                    resposta = WebUtility.HtmlDecode(node.Descendants().First(x => x.Attributes["class"] != null && x.Attributes["class"].Value.Equals("replycount")).InnerText);
+
+                    if (!string.IsNullOrEmpty(titulo))
+                    {
+                        dataGridView1.Rows.Add(titulo, postagem, exibicao, resposta, link);
+                    } 
+                }
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 4)
+            {
+                //Abrir a página web quando clicar no link da coluna
+                Process.Start(new ProcessStartInfo(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString()));
+            }
+        }
+    }
+}
